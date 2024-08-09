0cb6d85dc36edda71f7380424dcc8d498bb1004e
Ji-Un-Gil <wldns2577@gmail.com>
2024-08-09 15:43:07 +0900

    Fix: change FindMember logic

--- src/main/java/clpetition/backend/global/annotation/FindMember.java
+++ src/main/java/clpetition/backend/global/annotation/FindMember.java
@@ -1,14 +1,9 @@
 package clpetition.backend.global.annotation;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Target({ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : member")
+@Documented
 public @interface FindMember {
 }
