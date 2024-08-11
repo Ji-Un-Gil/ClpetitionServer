a7636b48f07e1bd752716bd35ce676510c225300
Ji-Un-Gil <wldns2577@gmail.com>
2024-08-12 01:48:54 +0900

    Docs: update server list

--- src/main/java/clpetition/backend/global/infra/docs/SwaggerConfig.java
+++ src/main/java/clpetition/backend/global/infra/docs/SwaggerConfig.java
@@ -6,16 +6,18 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "Clpetition API Docs",
                 description = "클피티션 API 명세서",
-                version = "v0.1.5"
+                version = "v0.1.6"
         )
 )
 @Configuration
@@ -25,6 +27,10 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        ArrayList<Server> servers = new ArrayList<>();
+        servers.add(new Server().url("https://www.clpetition-server.shop/").description("prod"));
+        servers.add(new Server().url("http://localhost:8080").description("dev"));
+
         SecurityScheme securityScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
@@ -35,6 +41,7 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("AccessToken", securityScheme))
+                .servers(servers)
                 .security(Arrays.asList(securityRequirement));
     }
 }
