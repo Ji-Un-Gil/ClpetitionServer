7d99f91783abfe98a60ef517927c55ea6766feb5
Ji-Un-Gil <wldns2577@gmail.com>
2024-08-15 19:30:39 +0900

    Chore: remove unuse query

--- src/main/java/clpetition/backend/member/repository/MemberRepository.java
+++ src/main/java/clpetition/backend/member/repository/MemberRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
     Boolean existsByNicknameAndRole(String nickname, Role role);
     Optional<Member> findBySocialId(String socialId);
     Optional<Member> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
