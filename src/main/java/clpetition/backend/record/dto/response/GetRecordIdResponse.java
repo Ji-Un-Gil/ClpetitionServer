17dc5d8d5cc54eedef668e47d8f84ffdae287c55
Ji-Un-Gil <wldns2577@gmail.com>
2024-09-03 21:13:38 +0900

    Refactor: update dto class to record

--- src/main/java/clpetition/backend/record/dto/response/GetRecordIdResponse.java
+++ src/main/java/clpetition/backend/record/dto/response/GetRecordIdResponse.java
@@ -1,18 +1,13 @@
 package clpetition.backend.record.dto.response;
 
 import clpetition.backend.record.docs.dto.response.GetRecordIdResponseSchema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class GetRecordIdResponse implements GetRecordIdResponseSchema {
-    private Long recordId;
-    private List<String> imageUrls;
+public record GetRecordIdResponse(
+        Long recordId,
+        List<String> imageUrls
+) implements GetRecordIdResponseSchema {
 }
