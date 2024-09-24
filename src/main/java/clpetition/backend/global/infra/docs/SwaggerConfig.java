7bd863d694c684f65b5bf2594b2d03547cc01722
Ji-Un-Gil <wldns2577@gmail.com>
2024-09-24 18:52:35 +0900

    Docs: update version

--- src/main/java/clpetition/backend/global/infra/docs/SwaggerConfig.java
+++ src/main/java/clpetition/backend/global/infra/docs/SwaggerConfig.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
         info = @Info(
                 title = "Clpetition API Docs",
                 description = "클피티션 API 명세서",
-                version = "v0.5.12"
+                version = "v0.6.0"
         )
 )
 @Configuration
